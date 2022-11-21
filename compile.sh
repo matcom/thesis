46f92b645cdf92fdebc53816bda9110a17abd2c8
Luiso <luise98cu@gamil.com>
2022-11-21 09:49:27 -0500

    First Deliver

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
-cd document/; echo q | pdflatex Thesis.tex 
+cd document/; echo q | pdflatex --shell-escape Thesis.tex 
 
 if [[ $1 = bib ]] 
 then
     cd document/; biber Thesis 
 
-    cd document/; echo q | pdflatex Thesis.tex 
-    cd document/; echo q | pdflatex Thesis.tex 
+    cd document/; echo q | pdflatex --shell-escape Thesis.tex 
+    cd document/; echo q | pdflatex --shell-escape Thesis.tex 
 fi
